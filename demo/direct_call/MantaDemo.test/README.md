1f06e9539da8613b7a518d3c85cbe905e22c1dc3
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-23 16:11:13 -0600

    Adding MantaDemo testing
    * based on StrelkaDemo as well as MantaDemo in mutect_tool
    * incomplete implementation

---
+++ demo/direct_call/MantaDemo.test/README.md
@@ -0,0 +1,5 @@
+Scripts here are designed to test individual steps of workflow inside of docker container
+using MantaDemo dataset.  Like the StrelkaDemo dataset, MantaDemo is a small dataset 
+which allows for the entire pipeline to be quickly exercised.  It was originally used with `manta_tool`, which 
+did not discover any variants in the StrelkaDemo data
+
---
+++ demo/direct_call/MantaDemo.test/project_data.sh
@@ -0,0 +1,52 @@
+# Define data for this run
+# * paths to external datasets
+# * all paths are specific to container
+
+
+# Mapping volumes
+# /data1: output volume
+# /data2: tumor BAM
+# /data3: normal BAM
+# /data4: location of parameters.  This should be mapped, even if using standard parameters installed in container
+# /data5: location of reference
+# /data6: location of DBSNP_DB
+# /data7: location of VEP_CACHE
+
+# on Katmai, assume for StrelkaDemo run:
+# /data1:/diskmnt/Projects/cptac_downloads_4/TinDaisy-Core
+# /data2,/data3,/data8 - StrelkaDemo.dat directory = /home/mwyczalk_test/Projects/TinDaisy/TinDaisy-Core/demo/test_data/MantaDemo.dat
+# /data4:/home/mwyczalk_test/Projects/TinDaisy/TinDaisy-Core/params
+# /data5,/data6 - MantaDemo.dat
+# /data7 is not mapped, not using VEP_CACHE
+
+# PARAMD="/usr/local/somaticwrapper/params"
+# PARAMD should be a mapped volume
+
+SAMPLE_NAME="MantaDemo"
+
+TUMOR_BAM="/data2/G15512.HCC1954.1.COST16011_region.bam"
+NORMAL_BAM="/data3/HCC1954.NORMAL.30x.compare.COST16011_region.bam"
+
+REF="/data5/Homo_sapiens_assembly19.COST16011_region.fa"
+
+PARAMD="/data4"
+STRELKA_CONFIG="$PARAMD/strelka.WES.ini"
+VARSCAN_CONFIG="$PARAMD/varscan.WES.ini"
+PINDEL_CONFIG="$PARAMD/pindel.WES.ini"
+STRELKA_VCF_FILTER_CONFIG="$PARAMD/vcf_filter_config-strelka.ini"
+VARSCAN_VCF_FILTER_CONFIG="$PARAMD/vcf_filter_config-varscan.ini"
+PINDEL_VCF_FILTER_CONFIG="$PARAMD/vcf_filter_config-pindel.ini"
+AF_FILTER_CONFIG="$PARAMD/af_filter_config.ini"
+CLASS_FILTER_CONFIG="$PARAMD/classification_filter_config.ini"
+
+# Centromere is also found with reference
+CENTROMERE_BED="/data5/ucsc-centromere.GRCh37.bed"
+
+# This needs to be created
+DBSNP_DB="/data6/dbsnp-MantaDemo.noCOSMIC.vcf.gz"
+
+#VEP_CACHE_DIR="/data7"
+ASSEMBLY="GRCh37"
+
+RESULTS_DIR="/data1/$SAMPLE_NAME"
+
