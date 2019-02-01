95026bfc86c735a93f83b8fc11f0f1eb8a739503
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-02-01 00:55:17 +0000

    VAF calculation testing

--- demo/direct_call/VCF_Filters/1b_run_vaf_filter-strelka2-snv.sh
+++ demo/direct_call/VCF_Filters/1b_run_vaf_filter-strelka2-snv.sh
@@ -1,7 +1,10 @@
 source project_data.sh
 
-# We require --pass_only for STRELKA2 because the output contains spurious non-pass variants 
+# We suggest --pass_only for STRELKA2 because the output contains spurious non-pass variants 
 # TODO: direct output somewhere
 
-bash run_vaf_filter.sh $STRELKA_SNV_VCF $STRELKA_VCF_FILTER_CONFIG --pass_only $@
+#bash run_vaf_filter.sh $STRELKA_SNV_VCF $STRELKA_VCF_FILTER_CONFIG $@ 
+
+echo Testing: removing header
+bash run_vaf_filter.sh $STRELKA_SNV_VCF $STRELKA_VCF_FILTER_CONFIG $@ | grep -v "^#"
 
--- src/vcf_filters/vaf_filter.py
+++ src/vcf_filters/vaf_filter.py
@@ -76,18 +76,11 @@ class TumorNormal_VAF(ConfigFileFilter):
     def filter_name(self):
         return self.name
 
-# TODO: provide support for strelka2 indels
-# * review Strelka1 read depth
-# * review strelka2 snv read depth - same as strelka1?
-# * review strelka2 indel read depth.  Look into DP, DP2 fields
-
-    def get_readcounts_strelka(self, VCF_record, VCF_data):
-        # pass VCF_record only to extract info (like ALT and is_snp) not available in VCF_data
-
-        if not VCF_record.is_snp:
-            raise Exception( "Only SNP calls supported for Strelka: " + str(VCF_record))
+    def get_snv_vaf_strelka(self, VCF_record, VCF_data):
+        # this tested for both Strelka1 and Strelka2
         # read counts, as dictionary. e.g. {'A': 0, 'C': 0, 'T': 0, 'G': 25}
-        tier=0   
+        # based on : https://github.com/Illumina/strelka/blob/v2.9.x/docs/userGuide/README.md#somatic-variant-allele-frequencies
+        tier=0  # corresponds to "tier1" 
         rc = {'A':VCF_data.AU[tier], 'C':VCF_data.CU[tier], 'G':VCF_data.GU[tier], 'T':VCF_data.TU[tier]}
 
         # Sum read counts across all variants. In some cases, multiple variants are separated by , in ALT field
@@ -95,12 +88,37 @@ class TumorNormal_VAF(ConfigFileFilter):
         #   Note we convert vcf.model._Substitution to its string representation to use as key
         rc_var = sum( [rc[v] for v in map(str, VCF_record.ALT) ] )
         rc_tot = sum(rc.values())
-        vaf = float(rc_var) / float(rc_tot) # Deal with rc_tot == 0
+        if rc_tot == 0:
+            vaf = 0.
+        else:
+            vaf = float(rc_var) / float(rc_tot) # Deal with rc_tot == 0
         if self.debug:
-            eprint("rc: %s, rc_var: %f, rc_tot: %f, vaf: %f" % (str(rc), rc_var, rc_tot, vaf))
+            eprint("rc: %s, rc_var: %d, rc_tot: %d, vaf: %f" % (str(rc), rc_var, rc_tot, vaf))
         return vaf
 
-    def get_readcounts_varscan(self, VCF_record, VCF_data):
+    def get_indel_vaf_strelka(self, VCF_record, VCF_data):
+        # this tested for Strelka2
+        # based on : https://github.com/Illumina/strelka/blob/v2.9.x/docs/userGuide/README.md#somatic-variant-allele-frequencies
+        tier=0  # corresponds to "tier1" 
+        rc = VCF_data.TAR[tier]  # RefCounts
+        ac = VCF_data.TAR[tier]  # AltCounts
+        if rc_tot == 0:
+            vaf = 0.
+        else:
+            vaf = float(ac) / float(ac + rc) # Deal with rc_tot == 0
+        if self.debug:
+            eprint("RefCounts: %d, AltCounts: %d, vaf: %f" % (rc, ac, vaf))
+        return vaf 
+
+    def get_vaf_strelka(self, VCF_record, VCF_data):
+        # If both are zero, avoid division by zero and return 0
+        # pass VCF_record only to extract info (like ALT and is_snp) not available in VCF_data
+        if VCF_record.is_snp:
+            return self.get_snv_vaf_strelka(VCF_record, VCF_data)
+        else:
+            return self.get_indel_vaf_strelka(VCF_record, VCF_data)
+
+    def get_vaf_varscan(self, VCF_record, VCF_data):
         # We'll take advantage of pre-calculated VAF
         # Varscan: CallData(GT=0/0, GQ=None, DP=96, RD=92, AD=1, FREQ=1.08%, DP4=68,24,1,0)
         ##FORMAT=<ID=FREQ,Number=1,Type=String,Description="Variant allele frequency">
@@ -110,7 +128,7 @@ class TumorNormal_VAF(ConfigFileFilter):
             eprint("VCF_data.FREQ = %s" % vaf)
         return float(vaf.strip('%'))/100.
 
-    def get_readcounts_mutect(self, VCF_record, VCF_data):
+    def get_vaf_mutect(self, VCF_record, VCF_data):
         # We'll take advantage of pre-calculated VAF
         # ##FORMAT=<ID=FA,Number=A,Type=Float,Description="Allele fraction of the alternate allele with regard to reference">
         vaf = VCF_data.FA
@@ -123,14 +141,15 @@ class TumorNormal_VAF(ConfigFileFilter):
         
         return float(vaf)
 
-    def get_readcounts_pindel(self, VCF_record, VCF_data):
+    def get_vaf_pindel(self, VCF_record, VCF_data):
         # read counts supporting reference, variant, resp.
         # If both are zero, avoid division by zero and return 0
         rc_ref, rc_var = VCF_data.AD
         rc_tot = float(rc_var + rc_ref)
         if rc_tot == 0:
-            return 0
-        vaf = rc_var / rc_tot
+            vaf = 0
+        else:
+            vaf = rc_var / rc_tot
         if self.debug:
             eprint("pindel VCF = %f" % vaf)
         return vaf
@@ -141,27 +160,27 @@ class TumorNormal_VAF(ConfigFileFilter):
             variant_caller = self.caller  # we permit the possibility that each line has a different caller
 
         if variant_caller == 'strelka':
-            return self.get_readcounts_strelka(VCF_record, data)
+            return self.get_vaf_strelka(VCF_record, data)
         elif variant_caller == 'varscan':
-            return self.get_readcounts_varscan(VCF_record, data)
+            return self.get_vaf_varscan(VCF_record, data)
         elif variant_caller == 'mutect':
-            return self.get_readcounts_mutect(VCF_record, data)
+            return self.get_vaf_mutect(VCF_record, data)
         elif variant_caller == 'pindel':
-            return self.get_readcounts_pindel(VCF_record, data)
+            return self.get_vaf_pindel(VCF_record, data)
         elif variant_caller == 'merged':
             # Caller is contained in 'set' INFO field
             merged_caller = VCF_record.INFO['set'][0]
             # TODO: It would be better to parse merged_caller to primary_caller, where merged set=A-B-C corresponds to primary_caller "A"
             if merged_caller == 'strelka':
-                return self.get_readcounts_strelka(VCF_record, data)
+                return self.get_vaf_strelka(VCF_record, data)
             elif merged_caller == 'varscan':
-                return self.get_readcounts_varscan(VCF_record, data)
+                return self.get_vaf_varscan(VCF_record, data)
             elif merged_caller == 'varindel':
-                return self.get_readcounts_varscan(VCF_record, data)
+                return self.get_vaf_varscan(VCF_record, data)
             elif merged_caller == 'strelka-varscan':
-                return self.get_readcounts_strelka(VCF_record, data)
+                return self.get_vaf_strelka(VCF_record, data)
             elif merged_caller == 'pindel':
-                return self.get_readcounts_pindel(VCF_record, data)
+                return self.get_vaf_pindel(VCF_record, data)
             else:
                 raise Exception( "Unknown caller in INFO set field: " + merged_caller)
         else:
