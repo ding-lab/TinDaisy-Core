9e8fdece10d5124237ec7be6a7e8a9121af771fb
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-04-01 10:49:21 -0500

    Docker image for tagged version

--- docker/docker_image.sh
+++ docker/docker_image.sh
@@ -3,8 +3,7 @@
 #DOCKER_IMAGE="mwyczalkowski/somatic-wrapper:cwl"
 
 # Note that we do not want to push this image.  Policy is to make all public images on katmai
-#DOCKER_IMAGE="cgc-images.sbgenomics.com/m_wyczalkowski/tindaisy-core:mutect"
-DOCKER_IMAGE="mwyczalkowski/tindaisy-core:pindel-chrom"
+DOCKER_IMAGE="mwyczalkowski/tindaisy-core:20190328"
 
 # If variable below evaluates to true, prevent `docker push`
 NO_PUSH=0
