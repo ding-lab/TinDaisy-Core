2951e9bb8aacf4d473052444db407f958094b5f1
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-04-20 08:45:12 -0500

    Bugfix in docker image names

--- docker/docker_image.core.sh
+++ docker/docker_image.core.sh
@@ -1,7 +1,7 @@
 # Image name associated with docker build and push of tindaisy-core
 
 # Policy is to make all public images on katmai
-DOCKER_IMAGE="mwyczalkowski/tindaisy-vep:20191108"
+DOCKER_IMAGE="mwyczalkowski/tindaisy-core:20191108"
 
 # If variable below evaluates to true, prevent `docker push`
 NO_PUSH=0
--- docker/docker_image.vep.sh
+++ docker/docker_image.vep.sh
@@ -2,7 +2,7 @@
 
 
 # Policy is to make all public images on katmai
-DOCKER_IMAGE="mwyczalkowski/tindaisy-vep"
+DOCKER_IMAGE="mwyczalkowski/tindaisy-vep:20200420"
 
 # If variable below evaluates to true, prevent `docker push`
 NO_PUSH=0
